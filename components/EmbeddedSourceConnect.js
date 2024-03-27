3af8f24707479728a76b9b25e9e2d5f6be24fefb
Hadley Killen <hadley@getcensus.com>
2024-03-27 10:10:36 -0700

    Dont hardcode localhost

--- components/EmbeddedSourceConnect.js
+++ components/EmbeddedSourceConnect.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react"
 
+import { censusBaseUrl } from "@utils/url"
+
 export default function EmbeddedSourceConnect({ connectLink, exitedConnectionFlow }) {
   useEffect(() => {
     const handleMessage = (event) => {
-      if (event.origin != "http://localhost:8080") {
+      if (event.origin != censusBaseUrl) {
         return
       }
       if (event.data.message === "EXITED_CONNECT_FLOW") exitedConnectionFlow()
