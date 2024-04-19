3bf373851d4506af9b41ca10450100cc84bb7e42
Arjit Jaiswal <arjitj2@gmail.com>
2024-04-19 11:34:21 -0400

    Update sidebar to say Import

--- pages/integrations/index.js
+++ pages/integrations/index.js
@@ -12,9 +12,27 @@ export default function Index() {
       </Head>
       <h2 className="text-2xl font-bold text-stone-700">Integrations</h2>
       <hr className="border-t border-stone-400" />
+      <h3 className="text-xl font-bold text-stone-700">Export</h3>
       <p className="italic text-stone-500">
         Activate your data by <Anchor href="https://www.getcensus.com">syncing</Anchor> it to the tools you
         use every day. It&apos;s time to bring your tea revenue to the next level!
+      </p>
+      <Link href="/integrations/crm">
+        <Button className="min-w-[300px] self-center" solid>
+          Connect your CRM
+          <i className="fa-solid fa-chevron-right ml-2" />
+        </Button>
+      </Link>
+      <Link href="/integrations/ads">
+        <Button className="min-w-[300px] self-center" solid>
+          Connect your ad platforms
+          <i className="fa-solid fa-chevron-right ml-2" />
+        </Button>
+      </Link>
+      <h3 className="text-xl font-bold text-stone-700">Import</h3>
+      <p className="italic text-stone-500">
+        Enrich your data in Tea Research International by <Anchor href="https://www.getcensus.com">syncing</Anchor> it 
+        from your data warehouse. Enhance your Tea Research today!
       </p>
       <Link href="/integrations/crm">
         <Button className="min-w-[300px] self-center" solid>
