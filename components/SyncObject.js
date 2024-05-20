485768add4009f4248e500d5a7c688fabd910191
Charles Liu <charles.liu0309@gmail.com>
2024-05-20 13:56:36 -0700

    fix .env.local exmple and mapping.from.data to mapping.to

--- components/SyncObject.js
+++ components/SyncObject.js
@@ -102,7 +102,7 @@ export function SyncObject({ workspaceAccessToken, sync, setSyncs, refetchSyncs,
       <p className="-mb-2 text-sm">These attributes will get synced...</p>
       <ul className="ml-6 flex grow list-disc flex-col gap-1 text-sm">
         {sync.mappings.map((mapping) => (
-          <li key={mapping.from.data}>{mapping.from.data}</li>
+          <li key={mapping.to}>{mapping.to}</li>
         ))}
       </ul>
       <div className="flex flex-row items-center justify-between gap-2">
