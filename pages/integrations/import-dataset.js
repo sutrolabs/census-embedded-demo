735b10af4bbc06f9a3cd5de3cfc9e2d9db938143
Charles Liu <charles.liu0309@gmail.com>
2024-05-21 16:58:19 -0700

    revert changes to import-dataset

--- pages/integrations/import-dataset.js
+++ pages/integrations/import-dataset.js
@@ -22,27 +22,36 @@ export default function ImportDataset({
   runs,
 }) {
   const [embedSourceFlow, setEmbedSourceFlow] = useState(true)
+  const [devMode, setDevMode] = useState(false)
 
   return (
     <>
       <Head>
         <title>Data Sources - Integrations - Census Embedded Demo App</title>
       </Head>
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between">
         <h2 className="text-2xl font-bold text-stone-700">Integrations / Import Dataset</h2>
-        <div className="flex items-center">
-          <Toggle
-            checked={embedSourceFlow}
-            onChange={() => setEmbedSourceFlow((prevCheck) => !prevCheck)}
-          ></Toggle>
-          <span className="px-2">Embed</span>
+        <div className="flex flex-row items-center gap-6">
+          <div className="flex items-center">
+            <Toggle
+              checked={embedSourceFlow}
+              onChange={() => setEmbedSourceFlow((prevCheck) => !prevCheck)}
+            ></Toggle>
+            <span className="px-2">Embed</span>
+          </div>
+          <div className="flex items-center">
+            <Toggle checked={devMode} onChange={() => setDevMode((prevCheck) => !prevCheck)}></Toggle>
+            <span className="px-2">Dev Mode</span>
+          </div>
         </div>
       </div>
       <hr className="border-t border-stone-400" />
       <p className="italic text-stone-500">
         <b>Note to customer:</b> On this page, the end user (your customer) can connect their source to your
         destination. Flip the &quot;Embed&quot; toggle above to swap between the embedded flow and redirect
-        flow the end user can use to connect their source and import their dataset.
+        flow the end user can use to connect their source and import their dataset. Flip the &quot;Dev
+        Mode&quot; toggle above to turn on or off tooltips of the requests being made when interacting with
+        the UX.
       </p>
       <p className="text-teal-400">Step 1: Connect your data source</p>
       <Source
@@ -53,10 +62,11 @@ export default function ImportDataset({
         sources={sources}
         setSources={setSources}
         refetchSources={refetchSources}
-        sourceConnectLinks={sourceConnectLinks}
         refetchSourceConnectLinks={refetchSourceConnectLinks}
+        sourceConnectLinks={sourceConnectLinks}
         sourceEmbedLinks={sourceEmbedLinks}
         embedSourceFlow={embedSourceFlow}
+        devMode={devMode}
         syncManagementLinks={syncManagementLinks}
         refetchSyncManagementLinks={refetchSyncManagementLinks}
         syncs={syncs}
@@ -77,6 +87,7 @@ export default function ImportDataset({
         refetchSourceConnectLinks={refetchSourceConnectLinks}
         sourceEmbedLinks={sourceEmbedLinks}
         embedSourceFlow={embedSourceFlow}
+        devMode={devMode}
         syncManagementLinks={syncManagementLinks}
         refetchSyncManagementLinks={refetchSyncManagementLinks}
         syncs={syncs}
@@ -97,6 +108,7 @@ export default function ImportDataset({
         refetchSourceConnectLinks={refetchSourceConnectLinks}
         sourceEmbedLinks={sourceEmbedLinks}
         embedSourceFlow={embedSourceFlow}
+        devMode={devMode}
         syncManagementLinks={syncManagementLinks}
         refetchSyncManagementLinks={refetchSyncManagementLinks}
         syncs={syncs}
@@ -117,6 +129,7 @@ export default function ImportDataset({
         refetchSourceConnectLinks={refetchSourceConnectLinks}
         sourceEmbedLinks={sourceEmbedLinks}
         embedSourceFlow={embedSourceFlow}
+        devMode={devMode}
         syncManagementLinks={syncManagementLinks}
         refetchSyncManagementLinks={refetchSyncManagementLinks}
         syncs={syncs}
