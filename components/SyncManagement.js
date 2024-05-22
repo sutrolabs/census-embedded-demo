3c5915a0e73c15bbb4bc26da1292e64a0b7fa62d
Charles Liu <charles.liu0309@gmail.com>
2024-05-21 23:10:46 -0700

    fixed flicker in EmbeddedFrame component (#34)
    * fixed flicker in EmbeddedFrame component
    
    * linting
    
    * revert changes to import-dataset

--- components/SyncManagement.js
+++ components/SyncManagement.js
@@ -1,9 +1,8 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 import Button from "@components/Button"
-import EmbeddedFrame from "@components/EmbeddedFrame"
+import SyncCreationWizard from "@components/SyncCreationWizard"
 import { SyncObject } from "@components/SyncObject"
-import { useSyncManagementLink } from "@hooks/use-sync-management-link"
 
 export default function SyncManagement({
   sourceId,
@@ -19,15 +18,6 @@ export default function SyncManagement({
   embedSourceFlow,
 }) {
   const [showCreateSyncWizard, setShowCreateSyncWizard] = useState(false)
-  const [syncManagementLink, resetSyncManagementLink] = useSyncManagementLink(
-    syncManagementLinks,
-    refetchSyncManagementLinks,
-    workspaceAccessToken,
-  )
-
-  const linkWithSourcePrepopulated = () => {
-    return syncManagementLink.uri + "&form_connection_id=" + sourceId + "&form_source_type=warehouse"
-  }
 
   const initiateSyncWizardFlow = () => {
     if (embedSourceFlow) {
@@ -35,32 +25,6 @@ export default function SyncManagement({
     } else {
       window.location.href = linkWithSourcePrepopulated()
     }
-  }
-
-  const SyncCreationWizard = () => {
-    return (
-      <EmbeddedFrame
-        connectLink={linkWithSourcePrepopulated()}
-        onExit={async (connectionDetails) => {
-          if (connectionDetails.status === "created") {
-            setSyncs((syncs) => [
-              ...syncs,
-              {
-                id: connectionDetails.details.id,
-                paused: true,
-                label: "Loading Sync",
-                source_attributes: { connection_id: sourceId },
-                mappings: [],
-              },
-            ])
-            await refetchSyncs()
-            // prepares a new link for the next sync creation
-            await resetSyncManagementLink()
-          }
-          setShowCreateSyncWizard(false)
-        }}
-      />
-    )
   }
 
   return (
@@ -82,7 +46,15 @@ export default function SyncManagement({
             />
           ))}
         {showCreateSyncWizard ? (
-          <SyncCreationWizard />
+          <SyncCreationWizard
+            sourceId={sourceId}
+            setSyncs={setSyncs}
+            refetchSyncs={refetchSyncs}
+            syncManagementLinks={syncManagementLinks}
+            refetchSyncManagementLinks={refetchSyncManagementLinks}
+            workspaceAccessToken={workspaceAccessToken}
+            setShowCreateSyncWizard={setShowCreateSyncWizard}
+          />
         ) : (
           <Button
             className="flex items-center justify-center rounded-md border border-indigo-500/40 bg-stone-50  px-5 py-8 text-xl
