fc28c86f9e0128e407f2732cbc704affdc6a67b2
Charles Liu <charles.liu0309@gmail.com>
2024-05-21 17:13:56 -0700

    added back table of contents

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,7 +6,7 @@ export default function Sidebar({ onLogOut }) {
     <>
       <div className="hidden md:block md:h-screen md:w-2/5 xl:w-1/4" />
       <div className="flex flex-row items-end justify-between gap-4 border-emerald-600 bg-emerald-400 p-2 md:fixed md:h-screen md:w-2/5 md:flex-col md:items-center md:justify-start md:p-6 xl:w-1/4">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 overflow-x-auto">
           <Link href="/">
             <a className="flex flex-row items-center gap-4 md:flex-col md:p-6">
               <i className="fa-solid fa-magnet text-2xl md:text-6xl" />
@@ -17,9 +17,11 @@ export default function Sidebar({ onLogOut }) {
           </Link>
           {/* Mobile Navbar */}
           <nav className="flex flex-row items-end gap-2 md:hidden">
-            <Item name="CRM" href="/export-crm" />
-            <Item name="Ad Platforms" href="/export-ads" />
-            <Item name="Import Dataset" href="/import-dataset" />
+            <Item name="Dashboard" href="/" />
+            <Item name="Integrations" href="/integrations" />
+            <Item name="CRM" href="/integrations/export-crm" />
+            <Item name="Ad Platforms" href="/integrations/export-ads" />
+            <Item name="Import Dataset" href="/integrations/import-dataset" />
           </nav>
         </div>
         <div className="hidden text-center text-lg italic text-stone-700 md:block md:text-2xl">
@@ -29,10 +31,12 @@ export default function Sidebar({ onLogOut }) {
           </abbr>
         </div>
         {/* Desktop Navbar */}
-        <nav className="hidden flex-col gap-4 self-start overflow-x-auto px-6 py-8 md:flex">
-          <Item name="Export to CRM" href="/export-crm" />
-          <Item name="Export to Ad Platforms" href="/export-ads" />
-          <Item name="Import Dataset to Marketing Magnet" href="/import-dataset" />
+        <nav className="hidden flex-col gap-4 self-start px-6 py-8 md:flex">
+          <Item name="Dashboard" href="/" />
+          <Item name="Integrations" href="/integrations" />
+          <Item nested name="Export to CRM" href="/integrations/export-crm" />
+          <Item nested name="Export to Ad Platforms" href="/integrations/export-ads" />
+          <Item nested name="Import Dataset to Marketing Magnet" href="/integrations/import-dataset" />
         </nav>
 
         <div className="flex items-center justify-between md:mt-auto md:w-full">
@@ -64,7 +68,7 @@ export default function Sidebar({ onLogOut }) {
   )
 }
 
-function Item({ name, href }) {
+function Item({ name, nested, href }) {
   const router = useRouter()
   const selected = router.asPath === href
 
@@ -72,8 +76,9 @@ function Item({ name, href }) {
     <Link href={href}>
       <a
         className="cursor-pointer border-b-2 border-transparent text-sm font-medium text-stone-900 hover:border-teal-800 hover:text-teal-800
-        data-[selected]:border-stone-900 data-[selected]:font-bold md:border-b-0 md:border-l-2 md:pl-2 md:text-2xl"
+        data-[nested]:ml-4 data-[selected]:border-stone-900 data-[selected]:font-bold md:border-b-0 md:border-l-2 md:pl-2 md:text-2xl"
         data-selected={selected ? "" : null}
+        data-nested={nested ? "" : null}
       >
         {name}
       </a>
---
+++ pages/integrations/index.js
@@ -0,0 +1,51 @@
+import Head from "next/head"
+import Link from "next/link"
+
+import { Anchor } from "@components/Anchor"
+import Button from "@components/Button"
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Integrations - Census Embedded Demo App</title>
+      </Head>
+      <h2 className="text-2xl font-bold text-stone-700">Integrations</h2>
+      <hr className="border-t border-stone-400" />
+      <h3 className="text-xl font-bold text-stone-700">Export</h3>
+      <p className="italic text-stone-500">
+        Activate your data by <Anchor href="https://www.getcensus.com">syncing</Anchor> it to the tools you
+        use every day. It&apos;s time to bring your tea revenue to the next level!
+      </p>
+      <Link href="/integrations/crm">
+        <Button className="min-w-[300px] self-center" solid>
+          Connect your CRM
+          <i className="fa-solid fa-chevron-right ml-2" />
+        </Button>
+      </Link>
+      <Link href="/integrations/ads">
+        <Button className="min-w-[300px] self-center" solid>
+          Connect your ad platforms
+          <i className="fa-solid fa-chevron-right ml-2" />
+        </Button>
+      </Link>
+      <h3 className="text-xl font-bold text-stone-700">Import</h3>
+      <p className="italic text-stone-500">
+        Enrich your data in Marketing Magnet by <Anchor href="https://www.getcensus.com">syncing</Anchor> it
+        from your data warehouse. Enhance your Marketing Research today!
+      </p>
+      <Link href="/integrations/crm">
+        <Button className="min-w-[300px] self-center" solid>
+          Connect your CRM
+          <i className="fa-solid fa-chevron-right ml-2" />
+        </Button>
+      </Link>
+      <Link href="/integrations/ads">
+        <Button className="min-w-[300px] self-center" solid>
+          Connect your ad platforms
+          <i className="fa-solid fa-chevron-right ml-2" />
+        </Button>
+      </Link>
+    </>
+  )
+}
