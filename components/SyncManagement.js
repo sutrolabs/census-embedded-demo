fb5e60626eda0f2cc89bbcd5ffc161510cdd7e66
Charles Liu <charles.liu0309@gmail.com>
2024-05-26 22:54:49 -0700

    hover over "Configure data to import" shows the API call to generate a sync management link (#47)
    * hover over "Configure data to import" shows the API call to generate a sync management link
    
    * updated redirect_uri to only show if not in embed mode

--- components/SyncManagement.js
+++ components/SyncManagement.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react"
 
 import Button from "@components/Button"
+import RequestTooltip from "@components/RequestTooltip"
 import SyncCreationWizard from "@components/SyncCreationWizard"
 import { SyncObject } from "@components/SyncObject"
 import { useSyncManagementLink } from "@hooks/use-sync-management-link"
+import { censusBaseUrl } from "@utils/url"
 
 export default function SyncManagement({
   sourceId,
+  type,
   syncManagementLinks,
   refetchSyncManagementLinks,
   workspaceAccessToken,
@@ -65,14 +68,49 @@ export default function SyncManagement({
         ) : (
           <Button
             className="flex items-center justify-center rounded-md border border-indigo-500/40 bg-stone-50  px-5 py-8 text-xl
-      shadow-sm"
+              shadow-sm"
             onClick={initiateSyncWizardFlow}
           >
-            <i className="fa-solid fa-plus mr-4" />
-            Configure data to import
+            <a id={`create-sync-${type}`}>
+              <i className="fa-solid fa-plus mr-4" />
+              Configure data to import
+            </a>
           </Button>
         )}
       </div>
+
+      <RequestTooltip
+        anchorSelect={`#create-sync-${type}`}
+        url={`${censusBaseUrl}/api/v1/sync_management_links`}
+        method="POST"
+        devMode={devMode}
+        headers={
+          <pre>
+            {JSON.stringify(
+              {
+                ["authorization"]: "Bearer <workspaceAccessToken>",
+                ["content-type"]: "application/json",
+              },
+              null,
+              2,
+            )}
+          </pre>
+        }
+        body={
+          !embedSourceFlow && (
+            <pre>
+              {JSON.stringify(
+                {
+                  redirect_uri: window.location.href,
+                },
+                null,
+                2,
+              )}
+            </pre>
+          )
+        }
+        link="https://developers.getcensus.com/api-reference/sync-management-links/create-a-new-sync-management-link"
+      />
     </>
   )
 }
