1d0575fc28a985343ec5d49e5bd0cdc785ea8351
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-13 12:55:27 -0500

    remove min-h for main wrapper of app

--- pages/_app.js
+++ pages/_app.js
@@ -217,7 +217,7 @@ function MainApplication({ Component, pageProps, workspaceAccessToken, onLogOut 
   }
 
   return (
-    <main className="relative flex min-h-screen w-screen flex-row">
+    <main className="relative flex h-screen w-screen flex-row">
       <Sidebar
         syncsLoading={syncsLoading}
         syncs={syncs}
