88467e94aff0808a7bb3b1462169bcfbbe7eef84
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-13 12:38:00 -0500

    update main layout to work with new header component

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,7 +1,7 @@
 export default function MainLayout({ children }) {
   return (
-    <div className="flex grow flex-col items-center">
-      <div className="mx-auto flex w-full  max-w-[1200px] flex-col gap-4 p-4 sm:p-8">{children}</div>
+    <div className="flex grow flex-col items-center bg-white">
+      <div className=" flex w-full flex-col">{children}</div>
     </div>
   )
 }
