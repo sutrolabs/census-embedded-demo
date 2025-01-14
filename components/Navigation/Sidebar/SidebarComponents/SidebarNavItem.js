000935c912978a731acb4242c29ee7d3a21a027a
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-14 16:21:57 -0500

    update style of sidebar nav item for interactivity

--- components/Navigation/Sidebar/SidebarComponents/SidebarNavItem.js
+++ components/Navigation/Sidebar/SidebarComponents/SidebarNavItem.js
@@ -9,13 +9,19 @@ export function Item({ name, href, icon: Icon }) {
   return (
     <Link
       href={href}
-      className={`flex cursor-pointer flex-row items-center gap-2 rounded p-2  text-sm font-medium leading-none  ${
+      className={`group flex cursor-pointer flex-row items-center gap-2 rounded  p-2 text-sm font-medium leading-none transition-all duration-75  ${
         isActive
           ? "  bg-neutral-200 text-neutral-700"
           : "border-transparent text-neutral-500 hover:bg-neutral-200/50 hover:text-neutral-700"
       }`}
     >
-      {Icon && <Icon className={`h-4 w-4 ${isActive ? "text-neutral-600" : "text-neutral-400"}`} />}
+      {Icon && (
+        <Icon
+          className={`h-4 w-4 transition-all duration-75 ${
+            isActive ? "text-neutral-600" : "text-neutral-400 group-hover:text-neutral-500"
+          }`}
+        />
+      )}
       <Text>{name}</Text>
     </Link>
   )
