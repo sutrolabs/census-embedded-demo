2879971d08786d907416b61422c85e9cdb5c5d1e
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-14 09:50:09 -0500

    update token entry and setup styling

--- components/Setup.js
+++ components/Setup.js
@@ -10,7 +10,7 @@ export function Setup({ workspaceAccessToken, setWorkspaceAccessToken }) {
       </Head>
       <div className="grid min-h-screen place-items-center bg-slate-100 p-8">
         <div className="flex max-w-xl flex-col gap-6 rounded-md border  border-slate-200 bg-white px-10 py-8">
-          <h1 className="text-2xl font-bold leading-none text-orange-700">Census Embedded API Setup</h1>
+          <h1 className="text-2xl font-bold leading-none">Census Embedded API Setup</h1>
           <TokenEntry
             workspaceAccessToken={workspaceAccessToken}
             setWorkspaceAccessToken={setWorkspaceAccessToken}
