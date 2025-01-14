750030590b3a54c8e91bd215b355cc411f5172fa
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-14 14:01:49 -0500

    updating styling for sidebar footer and toggle component

--- components/Navigation/Sidebar/SidebarComponents/SidebarFooter.js
+++ components/Navigation/Sidebar/SidebarComponents/SidebarFooter.js
@@ -1,49 +1,72 @@
 import Link from "next/link"
 import { Tooltip } from "react-tooltip"
-
+import { Text } from "@radix-ui/themes"
 import Toggle from "@components/Toggle"
+import { CentralLogOutIcon } from "@components/Icons/LogOut"
 
-export const SidebarFooter = ({ onLogOut, embedMode, setEmbedMode, devMode, setDevMode }) => (
-  <div className="flex flex-col items-center justify-between md:mt-auto md:w-full">
-    <div className="mb-3 hidden md:flex md:w-full md:flex-col md:gap-3">
-      <div className="flex items-center">
-        <span className="px-2">Embed</span>
-        <a id="embed-mode">
-          <Toggle checked={embedMode} onChange={() => setEmbedMode((prevCheck) => !prevCheck)} />
-        </a>
-        <Tooltip anchorSelect="#embed-mode">
-          Show Census UI flows embedded directly in the application, or redirect to the flows.
-        </Tooltip>
+const footerLinks = [
+  {
+    id: 1,
+    label: "Documentation",
+    href: "https://developers.getcensus.com/getting-started/introduction",
+    icon: "fa-brands fa-github",
+  },
+  {
+    id: 2,
+    label: "GitHub",
+    href: "https://github.com/sutrolabs/census-embedded-demo",
+    icon: "fa-solid fa-book",
+  },
+]
+
+export const SidebarFooter = ({ onLogOut, embedMode, setEmbedMode, devMode, setDevMode }) => {
+  const modeToggles = [
+    {
+      id: "embed-mode",
+      label: "Embedded Components",
+      checked: embedMode,
+      onChange: () => setEmbedMode((prevCheck) => !prevCheck),
+      anchorSelect: "#embed-mode",
+      tooltip: "Show Census UI flows embedded directly in the application, or redirect to the flows.",
+    },
+    {
+      id: "dev-mode",
+      label: "Developer Mode",
+      checked: devMode,
+      onChange: () => setDevMode((prevCheck) => !prevCheck),
+      anchorSelect: "#dev-mode",
+      tooltip: "Show tooltips of the Census API requests being made when interacting with the UX.",
+    },
+  ]
+  return (
+    <div className="flex flex-col justify-between md:w-full">
+      <div className="mb-3 hidden md:flex md:w-full md:flex-col md:gap-3">
+        {modeToggles.map((mode) => (
+          <div className="flex flex-row items-center justify-between" key={mode.id} id={mode.id}>
+            <Text className="text-sm font-medium">{mode.label}</Text>
+            <a>
+              <Toggle checked={mode.checked} onChange={mode.onChange} />
+            </a>
+            <Tooltip anchorSelect={mode.anchorSelect}>{mode.tooltip}</Tooltip>
+          </div>
+        ))}
+        <div className="hidden items-center gap-4 text-2xl md:flex md:flex-col">
+          {footerLinks.map((link) => (
+            <Link key={link.id} className="flex items-center gap-2 text-sm" href={link.href}>
+              <Text>{link.label}</Text>
+              <i className={link.icon} />
+            </Link>
+          ))}
+        </div>
       </div>
-      <div className="flex items-center">
-        <span className="px-2">Dev Mode</span>
-        <a id="dev-mode">
-          <Toggle checked={devMode} onChange={() => setDevMode((prevCheck) => !prevCheck)} />
-        </a>
-        <Tooltip anchorSelect="#dev-mode">
-          Show tooltips of the Census API requests being made when interacting with the UX.
-        </Tooltip>
-      </div>
-    </div>
-    <div className="flex items-center justify-between md:mt-auto md:w-full">
-      <button onClick={onLogOut} className="flex items-center gap-2 text-sm md:gap-3 md:text-2xl">
-        <i className="fa-solid fa-right-from-bracket mb-2 ml-2 md:mb-1" />
-        <p className="hidden md:block">Log out</p>
+
+      <button
+        onClick={onLogOut}
+        className="flex flex-row items-center justify-center gap-2 rounded-md border border-neutral-200 px-3 py-2 text-sm"
+      >
+        <CentralLogOutIcon className="h-4 w-4 text-neutral-500" />
+        <Text className="hidden text-neutral-600 md:block">Log out</Text>
       </button>
-      <div className="hidden items-center gap-4 text-2xl md:flex">
-        <Link
-          className="flex items-center gap-2 text-sm md:gap-3 md:text-2xl"
-          href="https://developers.getcensus.com/getting-started/introduction"
-        >
-          <i className="fa-solid fa-book" />
-        </Link>
-        <Link
-          className="flex items-center gap-2 text-sm md:gap-3 md:text-2xl"
-          href="https://github.com/sutrolabs/census-embedded-demo"
-        >
-          <i className="fa-brands fa-github" />
-        </Link>
-      </div>
     </div>
-  </div>
-)
+  )
+}
