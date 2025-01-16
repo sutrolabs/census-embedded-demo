f94a4b75104417cb329f4ed4124bd1c6dc806293
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-16 12:53:39 -0500

    update layout of integrations page to look more like empty state

--- pages/integrations/index.js
+++ pages/integrations/index.js
@@ -1,6 +1,5 @@
 import Link from "next/link"
 
-import { Anchor } from "@components/Anchor"
 import Button from "@components/Button"
 import Header from "@components/Structural/Header/Header"
 
@@ -8,22 +7,30 @@ export default function Index() {
   return (
     <>
       <Header title="Integrations" />
-      <div className="flex h-full flex-col gap-8 px-8 py-6">
-        <h3 className="text-xl font-bold text-neutral-700">Export</h3>
-        <p className="italic text-neutral-500">
-          Activate your data by <Anchor href="https://www.getcensus.com">syncing</Anchor> it to the tools you
-          use every day. It&apos;s time to bring your revenue to the next level!
-        </p>
+      <div className="flex h-full flex-col gap-6 px-8 py-6">
+        <div className="mx-auto flex w-full max-w-[1400px] flex-col items-center gap-5 rounded-lg bg-neutral-100 p-8">
+          <div className="flex flex-col items-center gap-2 leading-none">
+            <h3 className="text-xl font-bold text-neutral-700">Export</h3>
+            <p className="text-neutral-500">
+              Activate your data by{" "}
+              <Link href="https://www.getcensus.com" target="_blank">
+                syncing
+              </Link>{" "}
+              it to the tools you use every day. It&apos;s time to bring your revenue to the next level!
+            </p>
+          </div>
+          <div className="flex flex-row gap-4">
+            <Button className="min-w-[300px] self-center">
+              Connect your CRM
+              <i className="fa-solid fa-chevron-right ml-2 text-xs" />
+            </Button>
 
-        <Button className="min-w-[300px] self-center" solid>
-          Connect your CRM
-          <i className="fa-solid fa-chevron-right ml-2" />
-        </Button>
-
-        <Button className="min-w-[300px] self-center" solid>
-          Connect your ad platforms
-          <i className="fa-solid fa-chevron-right ml-2" />
-        </Button>
+            <Button className="min-w-[300px] self-center">
+              Connect your ad platforms
+              <i className="fa-solid fa-chevron-right ml-2" />
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   )
