1a9092b89944cf23e652d6cb226ff0a4faf346da
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-16 12:30:37 -0500

    Redo copy and form layout for setup
    move copy to main component out of the form and add help text to the form field

--- components/Setup.js
+++ components/Setup.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-
+import { Anchor } from "./Anchor"
 import TokenEntry from "@components/TokenEntry"
 
 export function Setup({ workspaceAccessToken, setWorkspaceAccessToken }) {
@@ -10,7 +10,11 @@ export function Setup({ workspaceAccessToken, setWorkspaceAccessToken }) {
       </Head>
       <div className="grid min-h-screen place-items-center bg-neutral-100 p-8">
         <div className="flex max-w-xl flex-col gap-6 rounded-md border  border-neutral-200 bg-white px-10 py-8">
-          <h1 className="text-2xl font-bold leading-none">Census Embedded API Setup</h1>
+          <h1 className="text-2xl font-bold leading-none">Welcome to the Census Embedded Demo</h1>
+          <p className="text-lg leading-normal">
+            Welcome! This demo app shows how your customer can use Census Embedded to import data from their
+            source to your desination and export data from your destination to their CRM and ad tools.
+          </p>
           <TokenEntry
             workspaceAccessToken={workspaceAccessToken}
             setWorkspaceAccessToken={setWorkspaceAccessToken}
