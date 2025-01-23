48b3453ad08c8c614e5277e93d742c3102ad9997
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-23 11:08:20 -0500

    update imports

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import Link from "next/link"
-
 import { Line } from "react-chartjs-2"
 
 import { Anchor } from "@components/Anchor"
--- pages/integrations/index.js
+++ pages/integrations/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import Link from "next/link"
-
 import { useRouter } from "next/router"
 
 import Button from "@components/Button"
