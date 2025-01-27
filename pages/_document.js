1367a56838663f78f64788f036807c79b2beeb49
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-01-27 11:37:24 -0500

    update global title and setup title

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="application-name" content="Census Embedded Demo App" />
+        <title>Census Embedded Demo App</title>
         <meta name="description" content="Demo application showcasing Census Embedded capabilities" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="shortcut icon" href="/logo.svg" />
