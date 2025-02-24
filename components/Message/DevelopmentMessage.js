343397b4ba0f313a820aadd8297b99b680da259e
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-02-24 14:55:53 -0500

    Update development message styling

--- components/Message/DevelopmentMessage.js
+++ components/Message/DevelopmentMessage.js
@@ -1,19 +1,9 @@
-import { Tooltip } from "react-tooltip"
-
-export default function DevelopmentMessage({ message, tooltipId, tooltipContent }) {
+export default function DevelopmentMessage({ message }) {
   return (
-    <div className="relative flex items-center gap-2 rounded-md border border-neutral-100 bg-white text-xs shadow-md">
+    <div className="relative flex items-center gap-2 rounded-md border border-neutral-100 bg-white text-xs shadow shadow-[#F36812]/20">
       <div className="z-20 flex flex-col px-3 py-2">
         <span className="font-medium text-[#F36812]">Dev Note</span>
         <span className="text-neutral-700">{message}</span>
-        {tooltipContent && (
-          <>
-            <span className="cursor-help text-neutral-400 hover:text-neutral-600" data-tooltip-id={tooltipId}>
-              ⓘ
-            </span>
-            <Tooltip id={tooltipId}>{tooltipContent}</Tooltip>
-          </>
-        )}
       </div>
       <div className="bg-development-warning absolute inset-0 -z-0 h-full w-full [mask-image:linear-gradient(transparent_0%,black)]" />
     </div>
