b7e2b422ed313f3b255ca89e410a814f60448461
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-03-03 11:13:48 -0500

    Add Embedded Provider to main app file

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,8 @@ import "@styles/globals.css"
 import { LinearScale, CategoryScale, LineElement } from "chart.js"
 import { PointElement, Tooltip, registry } from "chart.js"
 import dynamic from "next/dynamic"
-import { useState } from "react"
-import { useSessionStorage } from "usehooks-ts"
 
+import { useCensusEmbedded, CensusEmbeddedProvider } from "@components/Contexts/CensusEmbeddedContext"
 import Error_ from "@components/Error_"
 import Loading from "@components/Loading"
 import MainLayout from "@components/MainLayout"
@@ -19,28 +18,18 @@ registry.add(CategoryScale)
 registry.add(Tooltip)
 
 function Application({ Component, pageProps }) {
-  const [workspaceAccessToken, setWorkspaceAccessToken] = useSessionStorage("census_api_token", null)
-  const [loggedIn, setLoggedIn] = useSessionStorage("census-logged-in", false)
+  return (
+    <CensusEmbeddedProvider>
+      <ApplicationContent Component={Component} pageProps={pageProps} />
+    </CensusEmbeddedProvider>
+  )
+}
+
+function ApplicationContent({ Component, pageProps }) {
+  const { workspaceAccessToken, logOut } = useCensusEmbedded()
 
   return (
-    <>
-      {!workspaceAccessToken ? (
-        <Setup
-          workspaceAccessToken={workspaceAccessToken}
-          setWorkspaceAccessToken={setWorkspaceAccessToken}
-        />
-      ) : (
-        <MainApplication
-          Component={Component}
-          pageProps={pageProps}
-          workspaceAccessToken={workspaceAccessToken}
-          onLogOut={() => {
-            setWorkspaceAccessToken(null)
-            setLoggedIn(false)
-          }}
-        />
-      )}
-    </>
+    <>{!workspaceAccessToken ? <Setup /> : <MainApplication Component={Component} pageProps={pageProps} />}</>
   )
 }
 
@@ -48,9 +37,8 @@ export default dynamic(() => Promise.resolve(Application), {
   ssr: false,
 })
 
-function MainApplication({ Component, pageProps, workspaceAccessToken, onLogOut }) {
-  const [embedMode, setEmbedMode] = useState(true)
-  const [devMode, setDevMode] = useState(false)
+function MainApplication({ Component, pageProps }) {
+  const { workspaceAccessToken, embedMode, setEmbedMode, devMode, setDevMode, logOut } = useCensusEmbedded()
 
   const {
     loading: destinationsLoading,
@@ -222,7 +210,7 @@ function MainApplication({ Component, pageProps, workspaceAccessToken, onLogOut 
         syncs={syncs}
         runsLoading={runsLoading}
         runs={runs}
-        onLogOut={onLogOut}
+        onLogOut={logOut}
         embedMode={embedMode}
         setEmbedMode={setEmbedMode}
         devMode={devMode}
