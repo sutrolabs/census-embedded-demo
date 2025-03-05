3cd75823f63766d46fa3a65bd5111816d9d37923
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-03-05 08:15:06 -0500

    add new file to splice in destination logos

---
+++ data/connections/destination-connection-logos.js
@@ -0,0 +1,67 @@
+export const destination_connection_logos = [
+  {
+    label: "google_ads",
+    category: "ads",
+    logo: "/logos/connections/sources/snowflake-logo.svg",
+  },
+  {
+    label: "amazon_ads",
+    category: "ads",
+    logo: "/logos/connections/sources/databricks_logo.svg",
+  },
+  {
+    label: "constant_contact",
+    category: "ads",
+    logo: "/logos/connections/sources/redshift_logo.svg",
+  },
+  {
+    label: "criteo",
+    category: "ads",
+    logo: "/logos/connections/sources/athena_logo.svg",
+  },
+  {
+    label: "facebook_ads",
+    category: "ads",
+    logo: "/logos/connections/sources/bigquery_logo.svg",
+  },
+  {
+    label: "microsoft_ads",
+    category: "ads",
+    logo: "https://cdn.worldvectorlogo.com/logos/google-sheets-logo-icon.svg",
+  },
+  {
+    label: "pinterest_ads",
+    category: "ads",
+    logo: "https://www.gstatic.com/bricks/image/e5a399cad065b0dfe5f02ca2b23ddaf11065df4754d1d2fb3fa47fda35adc257.svg",
+  },
+  {
+    label: "radar",
+    category: "ads",
+    logo: "https://cdn.worldvectorlogo.com/logos/azure-2.svg",
+  },
+  {
+    label: "reddit_ads",
+    category: "ads",
+    logo: "/logos/connections/sources/clickhouse_logo.svg",
+  },
+  {
+    label: "snapchat_ads",
+    category: "ads",
+    logo: "/logos/connections/sources/confluent_logo.svg",
+  },
+  {
+    label: "tiktok_ads",
+    category: "ads",
+    logo: "/logos/connections/sources/elasticsearch_logo.svg",
+  },
+  {
+    label: "twitter_ads",
+    category: "ads",
+    logo: "/logos/connections/sources/greenplum_logo.svg",
+  },
+  {
+    label: "active_campaign",
+    category: "marketing",
+    logo: "/logos/connections/sources/greenplum_logo.svg",
+  },
+]
