8eeeb9b2786e3e4d9a3bfe04974be429734a5a49
Hadley Killen <hadley@getcensus.com>
2025-03-13 17:11:30 -0700

    Try setting the cache control headers in the next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ]
+  },
 }
